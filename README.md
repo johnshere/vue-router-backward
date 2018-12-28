f6f965a6915d80272ee3631064f6bc5229eacd79
liujiang <liujiang@si-tech.com.cn>
2018-12-28 18:45:22 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # vue-router-backward
+
 add backward function to vue-router，the function is difference from browser history
+
+## difference
+
+&emsp;&emsp;It will record the first router to the current router.And a route can only be recorded once. For example:
+
+> browser history：  
+>  A > B > C > B > D  
+> it`s record will change:  
+>  1: A  
+>  2: A > B  
+>  3: A > B > C  
+>  4: A > B  
+>  5: A > B > D
+
+## install
+
+```
+  npm install vue-router-backward
+```
+
+&emsp;&emsp;or
+
+```
+  yarn install vue-router-backward
+```
+
+## use
+
+&emsp;&emsp;First, add backward function to router.
+
+```
+import Vue from 'vue'
+import Router from 'vue-router'
+import backward from 'vue-router-backward'
+
+Vue.use(Router)
+
+let router = new Router({
+  routes: [
+    ...
+  ]
+})
+
+export default backward(router)
+```
+
+&emsp;&emsp;Second, trigger the backward function
+
+```
+export default {
+  name: 'xxx',
+  methods: {
+    goBefore () {
+      this.$router.backward()
+    }
+  }
+}
+```
+
+&emsp;&emsp;You can judge whether it's back or not in the previous page
+
+```
+  if (this.$route.params.isBack) {
+    this.transitionEffect = ''
+  }else {
+    this.transitionEffect = 'slideInRight'
+  }
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+export default function (router, options = {}) {
+  let storage = options.storage || sessionStorage // for refresh
+  let key = options.key || 'vue-router-backward' // key of persistence
+  // this will record the order of router, only once
+  let routesQuque
+  try {
+    routesQuque = JSON.parse(storage.getItem(key)) || []
+  } catch (e) {
+    routesQuque = []
+  }
+  // determine whether a route has occurred
+  let getIndexOf = path => {
+    for (let i in routesQuque) {
+      if (routesQuque[i].path === path) return i
+    }
+    return -1
+  }
+  let persist = storage => storage.setItem(key, JSON.stringify(routesQuque))
+  // back , send you to the previous route
+  router.backward = function (stepNum) {
+    if (stepNum !== 'number') stepNum = 1
+    stepNum = Math.abs(stepNum)
+    let index = routesQuque.length - 1 - stepNum
+    let route = routesQuque[0]
+    if (index > 0) route = routesQuque[index]
+
+    this.push({
+      name: route.name,
+      // path: route.path,
+      params: { isBack: true },
+      query: route.query
+    })
+  }
+
+  // record current route info, only once. if it had occurred, reset
+  router.afterEach(function (to, from) {
+    let index = getIndexOf(to.path)
+    if (index !== -1) {
+      routesQuque.splice(index)
+    }
+    routesQuque.push({
+      path: to.path,
+      name: to.name,
+      params: to.params,
+      query: to.query
+    })
+    persist(storage)
+  })
+  return router
+}
